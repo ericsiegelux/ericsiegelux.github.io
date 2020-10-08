ad6c10a2113260db913f1f0c3ff085020ce45945
Eriq Seagull <ericsiegel448@gmail.com>
2020-10-08 12:01:08 -0700

    Beautify CSS, HTML, Javascript

--- navbar.js
+++ navbar.js
@@ -1,35 +1,35 @@
-  function main(){
+function main() {
   var menuIconOpen = document.getElementsByClassName("open")[0];
   var menuIconClose = document.getElementsByClassName("close")[0];
   var menuItems = document.getElementsByClassName("menu");
-  
+
   menuIconClose.style.display = "none";
-  
-  menuIconOpen.onclick = function() {
+
+  menuIconOpen.onclick = function () {
     this.style.display = "none";
     menuIconClose.style.display = "flex";
     menuDisplay("show", menuItems);
   };
-  
-  menuIconClose.onclick = function() {
+
+  menuIconClose.onclick = function () {
     this.style.display = "none";
     menuIconOpen.style.display = "flex";
     menuDisplay("hide", menuItems);
   };
 }
 
-function menuDisplay(state, items){
-  if(state=="show"){
-    for(var i=0; i<items.length; i++){
+function menuDisplay(state, items) {
+  if (state == "show") {
+    for (var i = 0; i < items.length; i++) {
       items[i].classList.add("show");
     }
   } else {
-    for(var i=0; i<items.length; i++){
+    for (var i = 0; i < items.length; i++) {
       items[i].classList.remove("show");
     }
   }
 }
 
-window.onload = function(){
+window.onload = function () {
   main();
 }
